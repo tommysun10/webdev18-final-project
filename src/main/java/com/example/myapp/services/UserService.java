763f91d1d9a0e53c6b8670ea852b57ea5478072d
Saikrishna Chintalpelly <saichintal@ccis.neu.edu>
2018-08-10 15:29:00 -0400

    Added cors allow

--- src/main/java/com/example/myapp/services/UserService.java
+++ src/main/java/com/example/myapp/services/UserService.java
@@ -13,8 +13,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
+@CrossOrigin(origins="*")
 public class UserService {
     @Autowired
 	UserRepository userRepository;
@@ -102,6 +108,5 @@ public class UserService {
 			return userRepository.save(user);
 		}
 		return null;
-	}
-
+    }
 }
